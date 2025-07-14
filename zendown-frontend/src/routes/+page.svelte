f63a50fc26f3dc6e9b69fd600a4f77e48e762380
rishikanthc <rishikanthc@gmail.com>
2025-07-13 22:49:23 -0700

    bulk export of all notes

--- zendown-frontend/src/routes/+page.svelte
+++ zendown-frontend/src/routes/+page.svelte
@@ -16,6 +16,7 @@
 	import { toast } from 'svelte-sonner';
 	import { useSidebar } from '$lib/components/ui/sidebar/context.svelte.js';
 	import Collections from '$lib/components/Collections.svelte';
+	import ArrowBigDownDashIcon from '@lucide/svelte/icons/arrow-big-down-dash';
 	
 	let value = $state('');
 
@@ -781,9 +782,7 @@
 									{exportProgress.current} of {exportProgress.total}
 								</div>
 							{:else}
-								<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8l-4 4m0 0l-4-4m4 4V4"></path>
-								</svg>
+								<ArrowBigDownDashIcon class="w-4 h-4" />
 							{/if}
 						</Button>
 						
